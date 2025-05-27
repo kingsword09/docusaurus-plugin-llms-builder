6426b7bcd67aa48ba1b8d988a76e3ed330a38ce9
Kingsword <kingsword09@gmail.com>
2025-05-27 17:46:17 +0800

    chore: init project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import type { LoadContext, Plugin } from "@docusaurus/types";
+
+export default function defineDocusaurusPlugins(
+  context: LoadContext,
+  options: {}
+): Plugin<void> {
+  const { siteConfig } = context;
+
+  return {
+    name: "docusaurus-plugin-llms-builder",
+  };
+}
